3f180ba836034185736181f40de66a40a0310e43
Dayu Wang <dwk893@gmail.com>
2017-02-28 18:36:31 -0600

    Tutorial 6 Question 1 Source Code
    Dayu Wang - 02/28/17

---
+++ Tutorial_6_Assignment/Source/T6_-_Question_1/src/main/java/FeatureExtractor.java
@@ -0,0 +1,37 @@
+import org.openimaj.feature.local.list.LocalFeatureList;
+import org.openimaj.image.ImageUtilities;
+import org.openimaj.image.MBFImage;
+import org.openimaj.image.feature.local.engine.DoGSIFTEngine;
+import org.openimaj.image.feature.local.keypoints.Keypoint;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * Created by Dayu Wang on 02/28/2017.
+ */
+public class FeatureExtractor {
+    public static void main(String args[]) throws IOException {
+        String inputFolder = "data/";
+        String inputImage = "Joy2.jpg";
+        String outputFolder = "testData/";
+        String[] IMAGE_CATEGORIES = {"Anger", "Anticipation", "Joy"};
+
+        int input_class = 2;
+        MBFImage mbfImage = ImageUtilities.readMBF(new File(inputFolder + inputImage));
+        DoGSIFTEngine doGSIFTEngine = new DoGSIFTEngine();
+        LocalFeatureList<Keypoint> features = doGSIFTEngine.findFeatures(mbfImage.flatten());
+        FileWriter fw = new FileWriter(outputFolder + IMAGE_CATEGORIES[input_class] + ".txt");
+        BufferedWriter bw = new BufferedWriter(fw);
+        for (int i = 0; i < features.size(); i++) {
+            double c[] = features.get(i).getFeatureVector().asDoubleVector();
+            bw.write(input_class + ",");
+            for (int j = 0; j < c.length; j++) {
+                bw.write(c[j] + " ");
+            }
+            bw.newLine();
+        }
+        bw.close();
+    }
+}
